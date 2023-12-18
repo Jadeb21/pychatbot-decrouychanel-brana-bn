498cc75ab57e8709318da665d56b6142858c965f
MaelysCC <151128589+MaelysCC@users.noreply.github.com>
2023-12-18 19:40:42 +0100

    Add files via upload

--- MyFirstChatbot.py
+++ MyFirstChatbot.py
@@ -14,7 +14,7 @@ convert_file_lower_case(l,directory)
 
 text = text(l)
 
-word_occurrences_tf(text)
+tf = word_occurrences_tf(text)
 idf=idf(l)
 TD_IDF(l)
 td_idf=TD_IDF(l)
@@ -22,10 +22,27 @@ td_idf=TD_IDF(l)
 
 # Chercher les mots avec l'IDF le moins élever
 td_idf_min = TD_IDF_min(td_idf)
-#print(td_idf_min)
+print(td_idf_min)
 
 # Chercher les mots avec l'IDF le plus élevé
 td_idf_max = TD_IDF_max(idf)
 print(td_idf_max)
+
+
+repet_chirac = repet_chirac(td_idf)
+print(repet_chirac)
+
+mot_Nation(l, idf)
+
+menu()
+affichage()
+fonctionalite1 = fonctionnalite1()
+if fonctionalite1 == True :
+    question = question(fonctionnalite1)
+    print(question)
+tokeniser(question)
+terme_commun(question, l)
+
+
 
 
