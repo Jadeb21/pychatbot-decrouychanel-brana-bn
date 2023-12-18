b10691894a7f843698c12ab3a9991c16df589d72
Jadeb21 <151123659+Jadeb21@users.noreply.github.com>
2023-12-18 23:03:18 +0100

    Add files via upload

--- main.py
+++ main.py
@@ -1,11 +1,44 @@
 import os
 from Fonction import *
+import math
 
 directory = "./speeches"
-extension = "txt"
-l=list_of_files(directory, extension)
+extention = "txt"
+l=list_of_files(directory, extention)
 print_list(l)
 list_nom = extraire_nom(l)
 print(list_nom)
-letter=minuscule(l)
-print(letter)
+
+
+convert_file_lower_case(l,directory)
+
+
+text = text(l)
+
+tf = word_occurrences_tf(text)
+idf=idf(l)
+TD_IDF(l)
+td_idf=TD_IDF(l)
+#print(td_idf)
+
+# Chercher les mots avec l'IDF le moins élever
+td_idf_min = TD_IDF_min(td_idf)
+#print(td_idf_min)
+
+# Chercher les mots avec l'IDF le plus élevé
+td_idf_max = TD_IDF_max(idf)
+print(td_idf_max)
+
+repet_chirac = repet_chirac(td_idf)
+print(repet_chirac)
+
+mot_Nation(l, idf)
+mot_Climat(l, idf)
+menu()
+affichage()
+fonctionalite1 = fonctionnalite1()
+if fonctionalite1 == True :
+    question = question(fonctionnalite1)
+    print(question)
+tokenization_question(question)
+word_presence(question, l)
